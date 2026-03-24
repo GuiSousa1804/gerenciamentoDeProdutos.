4d4bef74cdd9dd9e086ba19aeb68a2199009ded3
Guilherme <guilhermesousa1804@gmail.com>
2026-03-24 19:56:09 -0300

    projeto cridado

---
+++ src/main/java/com/exercicio/produtoss/ProdutossApplication.java
@@ -0,0 +1,13 @@
+package com.exercicio.produtoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutossApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercicio/produtoss/ProdutossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio.produtoss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
